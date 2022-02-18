d262d1c6ce0e08c801200b1b5d6d9c187fff0cfa
ALUNO <ALUNO@DESKTOP-9HNBEJR>
2022-02-18 16:25:25 -0300

    Add project files.

---
+++ freq de num/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace freq_de_numeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a quantidade de valores a serem lidos: ");
+            int qtd = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[qtd];
+            while (qtd >= 1)
+            {
+                qtd--;
+                Console.Write("Digite os números: ");
+                int numeros = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+        }
+    }
+}
